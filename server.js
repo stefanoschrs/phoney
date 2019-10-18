f0dad12c05883b0c148cd8660754c404fcddd305
Stefanos Chrs <root@stefanoschrs.com>
2019-10-18 16:09:24 +0200

    Added some useless art on / :sunglasses:

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const modulesDir = process.env.MODULES_DIR || path.join(__dirname, 'modules')
 
 const app = express()
 
+app.get('/', (req, res) => res.send('<body style="margin:0;background-color:#000;color:#0F0;height:100vh;display:flex;justify-content:center;align-items:center"><pre>    ____  __  ______  _   __________  __\n   / __ \\/ / / / __ \\/ | / / ____/\\ \\/ /\n  / /_/ / /_/ / / / /  |/ / __/    \\  /\n / ____/ __  / /_/ / /|  / /___    / /\n/_/   /_/ /_/\\____/_/ |_/_____/   /_/</pre></body>'))
+
 fs
   .readdirSync('modules')
   .filter((module) => module.endsWith('.js'))
